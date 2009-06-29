bdad008e92573f186791032944c99f56ee2191b6
Jeff Aigner <enumeration@gmail.com>
2009-06-28 19:33:43 -0500

    Got to compile with no warnings or errors. Currently it is functioning as a simple HTTP 1.0 client.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = g++
 CFLAGS = -I /usr/local/include/boost-1_39/
 
-OBJECTS = main.o /lib/libboost_system-xgcc40-mt.a /lib/libboost_thread-xgcc40-mt-1_39.a
+OBJECTS = main.o /lib/libboost_system-xgcc40-mt.a
 
 main : $(OBJECTS)
 	$(CC) $(CFLAGS) $(OBJECTS)  -o main
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <boost/array.hpp>
+#include <istream>
+#include <ostream>
+#include <string>
 #include <boost/asio.hpp>
-
+#include <boost/bind.hpp>
 // blah blah
 
 using boost::asio::ip::tcp;
@@ -15,7 +17,17 @@ public:
     : resolver_(io_service),
       socket_(io_service)
   {
+    std::ostream request_stream(&request_);
+    request_stream << "GET " << path << " HTTP/1.0\r\n";
+    request_stream << "Host: " << server << "\r\n";
+    request_stream << "Accept: */*\r\n";
+    request_stream << "Connect: close\r\n\r\n";
 
+    tcp::resolver::query query(server, "http");
+    resolver_.async_resolve(query,
+			    boost::bind(&iclient::handle_resolve, this,
+					boost::asio::placeholders::error,
+					boost::asio::placeholders::iterator));
   }
 
 private:
@@ -32,22 +44,114 @@ private:
     }
   }
 
+  // Handle connection to the server
   void handle_connect(const boost::system::error_code &err,
 		      tcp::resolver::iterator endpoint_iterator)
   {
     if(!err) {
-      
+      boost::asio::async_write(socket_, request_,
+			       boost::bind(&iclient::handle_write_request, this,
+					   boost::asio::placeholders::error));
     } else if(endpoint_iterator != tcp::resolver::iterator()) {
       socket_.close();
       tcp::endpoint endpoint = *endpoint_iterator;
       socket_.async_connect(endpoint,
-			    boost::bind(&client::handle_connect, this
+			    boost::bind(&iclient::handle_connect, this,
 					boost::asio::placeholders::error, ++endpoint_iterator));
     } else {
       std::cout << "Error: " << err.message() << "\n";
     }
   }
-}
+
+  void handle_write_request(const boost::system::error_code &err)
+  {
+    if(!err) {
+      boost::asio::async_read_until(socket_, response_, "\r\n",
+				    boost::bind(&iclient::handle_read_status_line, this,
+						boost::asio::placeholders::error));
+    } else {
+      std::cout << "Error: " << err.message() << "\n";
+    }
+  }
+
+  void handle_read_status_line(const boost::system::error_code &err)
+  {
+    if(!err) {
+      // check code
+      std::istream response_stream(&response_);
+      std::string http_version;
+      response_stream >> http_version;
+      unsigned int status_code;
+      response_stream >> status_code;
+      std::string status_message;
+      std::getline(response_stream, status_message);
+      if(!response_stream || http_version.substr(0, 5) != "HTTP/") {
+	std::cout << "Invalid response\n";
+	return;
+      }
+
+      if(status_code != 200) {
+	std::cout << "Reponse returned with status code ";
+	std::cout << status_code << "\n";
+	return;
+      }
+
+      // Read the headers, terminated by blank line.
+      boost::asio::async_read_until(socket_, response_, "\r\n\r\n",
+				    boost::bind(&iclient::handle_read_headers, this,
+						       boost::asio::placeholders::error));
+	  
+    } else {
+      std::cout << "Error: " << err.message() << "\n";
+    }
+  }
+
+  // handle the header of the response
+  void handle_read_headers(const boost::system::error_code &err)
+  {
+    if(!err) {
+      // Process the header
+      std::istream response_stream(&response_);
+      std::string header;
+      while(std::getline(response_stream, header) && header != "\r")
+	std::cout << header << "\n";
+      std::cout << "\n";
+
+      // Write content we have to output
+      if(response_.size() > 0)
+	std::cout << &response_;
+
+      // read remaining data to EOF
+      boost::asio::async_read(socket_, response_,
+			      boost::asio::transfer_at_least(1),
+			      boost::bind(&iclient::handle_read_content, this,
+					  boost::asio::placeholders::error));
+	  
+    } else {
+      std::cout << "Error: " << err.message() << "\n";
+    }
+  }
+
+  void handle_read_content(const boost::system::error_code &err)
+  {
+    if(!err) {
+      std::cout << &response_;
+
+      boost::asio::async_read(socket_, response_,
+			      boost::asio::transfer_at_least(1),
+			      boost::bind(&iclient::handle_read_content, this,
+					  boost::asio::placeholders::error));
+    } else {
+      std::cout << "Error: " << err.message() << "\n";
+    }
+  }
+
+  tcp::resolver resolver_;
+  tcp::socket socket_;
+  boost::asio::streambuf request_;
+  boost::asio::streambuf response_;
+
+};
 
 void auth(tcp::socket *socket) {
   boost::asio::streambuf request;
@@ -63,44 +167,13 @@ void auth(tcp::socket *socket) {
 
 int main(int argc, char *argv[]) {
   try {
-    if(argc != 2) {
-      std::cerr << "Usage: client <host>" << std::endl;
+    if(argc != 3) {
+      std::cout << "Usage: async_client <server> <path>\n";
+      return 1;
     }
-
     boost::asio::io_service io_service;
-
-    tcp::resolver resolver(io_service);
-    tcp::resolver::query query(argv[1], "6667");
-    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-    tcp::resolver::iterator end;
-
-    tcp::socket socket(io_service);
-    boost::system::error_code error = boost::asio::error::host_not_found;
-
-    while(error && endpoint_iterator != end) {
-      socket.close();
-      socket.connect(*endpoint_iterator++, error);
-    }
-    if(error)
-      throw boost::system::system_error(error);
-
-    // Authorize with the network
-    auth(&socket);
-
-    for (;;) {
-      boost::array<char, 128> buf;
-      boost::system::error_code error;
-
-      size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-      if(error == boost::asio::error::eof)
-	break;
-      else if(error)
-	throw boost::system::system_error(error);
-
-      std::cout.write(buf.data(), len);
-
-    }
+    iclient c(io_service, argv[1], argv[2]);
+    io_service.run();
   }
   catch (std::exception &e) {
     std::cerr << e.what() << std::endl;
